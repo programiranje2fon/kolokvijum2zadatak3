0459c6834f540a374b7cc89f619356106aadc8aa
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-21 19:26:42 +0100

    Promenjen naziv klase koja poziva ispravku koda.

--- src/ispravka_koda/ProbaStringIspisivac4.java
+++ src/ispravka_koda/ProbaStringIspisivac4.java
@@ -1,6 +1,6 @@
 package ispravka_koda;
 
-public class TestStringIspisivac4 {
+public class ProbaStringIspisivac4 {
 
 	public static void main(String[] args) {
 		String[] niz = new String[] { "BELO", "CRNO", "ZUTO", "SIVO" };
